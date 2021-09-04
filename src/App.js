d6e8ea0145b05487a80bd6e6cc815694cff7e3a8
sagardh287 <sagardhanawale1@gmail.com>
2021-09-04 01:44:30 -0700

    thid commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Homescreen from './screen/Homescreen';
 function App() {
     return (
         <div>
-            {/* <Homescreen/> */}
-            <h1>sagar</h1>
+            <Homescreen/>
+            {/* <h1>sagar</h1> */}
         </div>
     )
 }

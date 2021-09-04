1c65417b3761567dd3986c60f2a6f32a3e5a8e0f
sagardh287 <sagardhanawale1@gmail.com>
2021-09-04 02:07:01 -0700

    thid commit

--- src/screen/Homescreen.js
+++ src/screen/Homescreen.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Footer from './Footer'
 
 export default function Homescreen() {
     return (
         <div  className="bgimg">
-        <h1>sagar</h1>
+        <Footer/>
         </div>
         )
 }

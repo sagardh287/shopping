177c7a113c14f34b89429c873992a06405dc42ac
sagardh287 <sagardhanawale1@gmail.com>
2021-09-04 02:49:19 -0700

    navbar

--- src/screen/Main.js
+++ src/screen/Main.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Cardlist from '../component/Cardlist'
-// import image from '../data/bgimage.jpg'
+import image from "../data/bgimage.jpg"
 
 export default function Main() {
-    // const [responseData,setResponseData]=useState(["1","2","3","4","5","6"])
      
     return (
         <div className="container mb-3">
             <div class="row row-cols-2 row-cols-md-5 g-4">
-               <Cardlist/>
+               <Cardlist image={image}/>
             </div>
         </div>
     )

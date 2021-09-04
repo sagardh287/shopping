ed201c3270c11c71e637bfaed4a46e42276b9658
sagardh287 <sagardhanawale1@gmail.com>
2021-09-04 02:53:21 -0700

    navbar

--- src/screen/Main.js
+++ src/screen/Main.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import Cardlist from '../component/Cardlist'
-import image from "../data/bgimage.jpg"
 
 export default function Main() {
      
     return (
         <div className="container mb-3">
             <div class="row row-cols-2 row-cols-md-5 g-4">
-               <Cardlist image={image}/>
+               <h1>sagar</h1>s
             </div>
         </div>
     )

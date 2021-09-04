85f476ced1e50357619d5b19fb704e6777052197
sagardh287 <sagardhanawale1@gmail.com>
2021-09-04 03:17:06 -0700

    navbar

--- src/screen/Main.js
+++ src/screen/Main.js
@@ -1,17 +1,36 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import Cardlist from '../component/Cardlist'
 import image from '../data/bgimage.jpg'
 
 export default function Main() {
-    const [responseData,setResponseData]=useState([1,2,3,4,5,6])
-     
+    const [responseData,setResponseData]=useState([])
+    
+
+    useEffect(()=>{
+        setResponseData([{
+            id:"1",
+            name:"sagar"
+        }
+        ,{
+            id:"2",
+           name:"sagar"
+       },
+       {
+           id:"3",
+           name:"sagar"
+       },
+       {
+           id:"4",
+           name:"sagar"
+       }])
+    },[])
     return (
         <div className="container mb-3">
             <div class="row row-cols-2 row-cols-md-5 g-4">
                 {/* <Cardlist image={image}/> */}
                 {
                     responseData.map((data)=>(
-                        <div key={data}>
+                        <div key={data.id}>
                             <Cardlist image={image} />
                             <Cardlist image={image} />
                             <Cardlist image={image} />

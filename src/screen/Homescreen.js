6f38f0c70c7506a40332f048ea2ae09833b35fc6
sagardh287 <sagardhanawale1@gmail.com>
2021-09-04 01:12:58 -0700

    thid commit

--- src/screen/Homescreen.js
+++ src/screen/Homescreen.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import Navbar from '../component/Navbar'
-import Navbar2 from '../component/Navbar2'
-import Header from '../component/Header'
+
 
 import './homescreen.css'
-import Cardlist from '../component/Cardlist'
-import Main from '../screen/Main'
-import Footer from './Footer'
 
 export default function Homescreen() {
     return (
         <div  className="bgimg">
-            {/* <Navbar/>
-            <Header/>
-             */}
-            <Footer/>
+        <h1>sagar</h1>
         </div>
         )
 }

281092425ab67dd67ba20897cea61912e31a0abe
sagardh287 <sagardhanawale1@gmail.com>
2021-09-04 02:46:22 -0700

    navbar

--- src/screen/Main.js
+++ src/screen/Main.js
@@ -1,32 +1,14 @@
-import React,{ useState} from 'react'
+import React from 'react'
 import Cardlist from '../component/Cardlist'
-import image from '../data/bgimage.jpg'
-// import axios from 'axios'
+// import image from '../data/bgimage.jpg'
 
 export default function Main() {
-    const [responseData,setResponseData]=useState(["1","2","3","4","5","6"])
-    // const getdata=async ()=>{
-    //     await axios.get("/product").then((response)=>{
-    //         setResponseData(response.data)
-    //     })
-    //  }
-    //  useEffect(() => {
-    //      getdata();
-    //  },[])
+    // const [responseData,setResponseData]=useState(["1","2","3","4","5","6"])
      
     return (
         <div className="container mb-3">
             <div class="row row-cols-2 row-cols-md-5 g-4">
-                {
-                    responseData.map((data)=>(
-                        <Cardlist image={image} name="lenovo"/>
-                    ))
-                }
-                {
-                    responseData.map((data)=>(
-                        <Cardlist image={image} name="lenovo"/>
-                    ))
-                }
+               <Cardlist/>
             </div>
         </div>
     )

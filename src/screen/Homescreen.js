642a3d1921e4c43b23a21246eba7a8883eb8e740
sagardh287 <sagardhanawale1@gmail.com>
2021-09-04 02:38:41 -0700

    navbar

--- src/screen/Homescreen.js
+++ src/screen/Homescreen.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
+import Header from '../component/Header'
 import Footer from './Footer'
 
 export default function Homescreen() {
     return (
         <div  className="bgimg">
         <Navbar/>
+        <Header/>
         <Footer/>
         </div>
         )
